7adbd918ee4443d64dfa71be8d83a9bfaf77d51d
shivendranitk <shivam.shivendra24@gmail.com>
2021-01-15 09:40:29 +0530

    Initial Commit

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipetronik.Calibration
+{
+    class Calculator
+    {
+        public static int Add(int x,int y)
+        {
+            return x + y;
+
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+
+        }
+    }
+}
